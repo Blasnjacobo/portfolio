c253dc0c56b1e08263a24e8d312efbdb85723d70
Blasnjacobo <blasnjacobo05@gmail.com>
2024-01-24 13:22:08 -0700

    adding loteria

--- src/pages/components/MyPortfolio/RepositoryData.js
+++ src/pages/components/MyPortfolio/RepositoryData.js
@@ -1,85 +1,90 @@
+/* eslint-disable semi */
+/* eslint-disable quotes */
+/* eslint-disable comma-dangle */
 /* eslint-disable no-unused-vars */
-let RepoData = []
-export default RepoData =
-[
+let RepoData = [];
+export default RepoData = [
   {
     id: 1,
-    title: 'Movie section',
-    app: 'https://blasnjacobo.github.io/movie-series/',
-    repo: 'https://github.com/Blasnjacobo/movie-series',
-    img: 'https://github.com/Blasnjacobo/movie-series/blob/main/src/components/movie-series.png?raw=true',
-    description: 'Movie platform created with react'
+    title: "Movie section",
+    app: "https://blasnjacobo.github.io/movie-series/",
+    repo: "https://github.com/Blasnjacobo/movie-series",
+    img: "https://github.com/Blasnjacobo/movie-series/blob/main/src/components/movie-series.png?raw=true",
+    description: "Movie platform created with react",
   },
   {
     id: 2,
-    title: 'Shopping Cart',
-    app: 'https://blasnjacobo.github.io/shopping-cart',
-    repo: 'https://github.com/Blasnjacobo/shopping-cart',
-    img: 'https://github.com/Blasnjacobo/shopping-cart/blob/main/public/shopping-cart.png?raw=true',
-    description: 'React project with Typescript and Bootstrap'
+    title: "Shopping Cart",
+    app: "https://blasnjacobo.github.io/shopping-cart",
+    repo: "https://github.com/Blasnjacobo/shopping-cart",
+    img: "https://github.com/Blasnjacobo/shopping-cart/blob/main/public/shopping-cart.png?raw=true",
+    description: "React project with Typescript and Bootstrap",
   },
   {
     id: 3,
-    title: 'Travel Advisor',
-    app: 'https://blasnjacobo.github.io/Travel-advisor',
-    repo: 'https://github.com/Blasnjacobo/Travel-advisor',
-    img: 'https://github.com/Blasnjacobo/Travel-advisor/blob/main/public/Travel-advisor.png?raw=true',
-    description: 'React with material ui, and Google-map-react,'
+    title: "Travel Advisor",
+    app: "https://blasnjacobo.github.io/Travel-advisor",
+    repo: "https://github.com/Blasnjacobo/Travel-advisor",
+    img: "https://github.com/Blasnjacobo/Travel-advisor/blob/main/public/Travel-advisor.png?raw=true",
+    description: "React with material ui, and Google-map-react,",
   },
   {
     id: 4,
-    title: 'MERN FULLSTACK website',
-    app: 'https://6572281633382a0ae5ab9e41--preeminent-capybara-9e01c4.netlify.app/',
-    repo: 'https://github.com/Blasnjacobo/Dashboard',
-    img: 'https://github.com/Blasnjacobo/Dashboard/blob/main/website_pic.png?raw=true',
-    description: 'I am still working on optimizing the cold start after clicking the login button, if this page hasnt been active for a while it could take up to 1 minute to load, thanks for your comprehension'
+    title: "MERN FULLSTACK website",
+    app: "https://6572281633382a0ae5ab9e41--preeminent-capybara-9e01c4.netlify.app/",
+    repo: "https://github.com/Blasnjacobo/Dashboard",
+    img: "https://github.com/Blasnjacobo/Dashboard/blob/main/website_pic.png?raw=true",
+    description:
+      "I am still working on optimizing the cold start after clicking the login button, if this page hasnt been active for a while it could take up to 1 minute to load, thanks for your comprehension",
   },
   {
     id: 5,
-    title: 'Translation',
-    app: 'https://blasnjacobo.github.io/translator/',
-    repo: 'https://github.com/Blasnjacobo/translator',
-    img: 'https://github.com/Blasnjacobo/translator/blob/main/src/translatorApp.png?raw=true',
-    description: 'Translator using react, and Text Translator Api fetch'
+    title: "LOTERIA",
+    app: "https://blasnjacobo.github.io/loteria-personalizada/",
+    repo: "https://github.com/Blasnjacobo/loteria-personalizada",
+    img: "https://github.com/Blasnjacobo/tenzies/blob/main/src/LoteriaJuego.png?raw=true",
+    description: "Personaliza tu Loteria, programa usando REACT y Typescript",
   },
   {
     id: 6,
-    title: 'Metal and Wood Products website',
-    app: 'https://metalandwoodproducts.com/',
-    repo: '',
-    img: 'https://github.com/Blasnjacobo/tenzies/blob/main/src/image.png?raw=true',
-    description: 'Companys website'
+    title: "Metal and Wood Products website",
+    app: "https://metalandwoodproducts.com/",
+    repo: "",
+    img: "https://github.com/Blasnjacobo/tenzies/blob/main/src/image.png?raw=true",
+    description: "Companys website",
   },
   {
     id: 7,
-    title: 'Personal Project',
-    app: '',
-    repo: '',
-    img: 'https://github.com/Blasnjacobo/portfolio/blob/main/src/pages/components/MyPortfolio/Flixprop.png?raw=true',
-    description: 'Creating a website to sell updated cinema merchandise, still in process...'
+    title: "Personal Project",
+    app: "",
+    repo: "",
+    img: "https://github.com/Blasnjacobo/portfolio/blob/main/src/pages/components/MyPortfolio/Flixprop.png?raw=true",
+    description:
+      "Creating a website to sell updated cinema merchandise, still in process...",
   },
   {
     id: 8,
-    title: 'Notes',
-    app: 'https://blasnjacobo.github.io/notes-app/',
-    repo: 'https://github.com/Blasnjacobo/notes-app',
-    img: 'https://github.com/Blasnjacobo/notes-app/blob/main/src/notes-app.png?raw=true',
-    description: 'Creating a basic notes app using react'
+    title: "Notes",
+    app: "https://blasnjacobo.github.io/notes-app/",
+    repo: "https://github.com/Blasnjacobo/notes-app",
+    img: "https://github.com/Blasnjacobo/notes-app/blob/main/src/notes-app.png?raw=true",
+    description: "Creating a basic notes app using react",
   },
   {
     id: 9,
-    title: 'Meme generator',
-    app: 'https://blasnjacobo.github.io/meme-generator/',
-    repo: 'https://github.com/Blasnjacobo/meme-generator/tree/main',
-    img: 'https://raw.githubusercontent.com/Blasnjacobo/meme-generator/main/src/Meme/meme-generator.png',
-    description: 'Created using React and fetch for API https://api.imgflip.com/get_memes'
+    title: "Meme generator",
+    app: "https://blasnjacobo.github.io/meme-generator/",
+    repo: "https://github.com/Blasnjacobo/meme-generator/tree/main",
+    img: "https://raw.githubusercontent.com/Blasnjacobo/meme-generator/main/src/Meme/meme-generator.png",
+    description:
+      "Created using React and fetch for API https://api.imgflip.com/get_memes",
   },
   {
     id: 10,
-    title: 'Quizzes',
-    app: 'https://blasnjacobo.github.io/quiz-app',
-    repo: 'https://github.com/Blasnjacobo/quiz-app',
-    img: 'https://github.com/Blasnjacobo/quiz-app/blob/main/src/assets/quizApp.png?raw=true',
-    description: 'Quizz App using react, Tailwind and Router'
-  }
-]
+    title: "Quizzes",
+    app: "https://blasnjacobo.github.io/quiz-app",
+    repo: "https://github.com/Blasnjacobo/quiz-app",
+    img: "https://github.com/Blasnjacobo/quiz-app/blob/main/src/assets/quizApp.png?raw=true",
+    description: "Quizz App using react, Tailwind and Router",
+  },
+];
